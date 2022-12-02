202997798b75dfd74dd78292d4a1fb2785bc5377
Timur <MuratovTim3@gmail.com>
2022-12-02 23:43:15 +0300

    First steps

---
+++ script/load-animation.js
@@ -0,0 +1,24 @@
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elementTop = rect.top;
+    var elementBottom = rect.bottom;
+
+    var isVisible = elementTop < window.innerHeight && elementBottom >= 0;
+    return isVisible;
+}
+
+let welcometext = document.getElementById("welcome-text");
+let animationTargets = document.querySelectorAll(".animation-trigger");
+
+if (isScrolledIntoView(welcometext)){
+    welcometext.classList.add("animated");
+}
+
+document.onscroll = (event) => { 
+    animationTargets.forEach(element => {
+        if (isScrolledIntoView(element)){
+            element.classList.add("animated");
+        }
+    });
+    
+};
